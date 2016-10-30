313240293629b636839957f19dd0706268909892
Vinh Tran <daivinhtran.vt@gmail.com>
2016-10-29 23:42:08 -0400

    refractor duplicate code to helper funciton

--- Week 1/LongestPalindrome.java
+++ Week 1/LongestPalindrome.java
@@ -11,62 +11,31 @@ Examples:
  */
 
 public class LongestPalindrome {
+    private static  int lo, maxLen;
+
     public static String longestPalindrome(String s) {
-        int maxLen = 0;
-        int maxLeft = 0;
-        int maxRight = 0;
-        int left = 0;
-        int right = 0;
+        if (s.length() < 2) return s;
 
         for (int i = 0; i < s.length() - 1; i++) {
-            left = i;
-            right = i;
-
-
-            while (left >= 0 && right < s.length()) {
-                if (s.charAt(left) == s.charAt(right)) {
-                    left--;
-                    right++;
-                } else {
-                    break;
-                }
-            }
-
-            left++;
-            right--;
-            if (maxLen < (right - left + 1)) {
-                maxLeft = left;
-                maxRight = right;
-                maxLen = maxRight - maxLeft + 1;
-            }
-
-
-            left = i;
-            right = i;
-
-            if (s.charAt(right + 1) == s.charAt(left)) {
-                right++;
-                while (left >= 0 && right < s.length()) {
-                    if (s.charAt(left) == s.charAt(right)) {
-                        left--;
-                        right++;
-                    } else {
-                        break;
-                    }
-                }
-
-                left++;
-                right--;
-                if (maxLen < (right - left + 1)) {
-                    maxLeft = left;
-                    maxRight = right;
-                    maxLen = maxRight - maxLeft + 1;
-                }
-            }
+            helper(s, i, i);
+            helper(s, i, i + 1);
         }
 
-        return s.substring(maxLeft, maxRight + 1);
+        return s.substring(lo, lo + maxLen);
     }
+
+    public static void helper(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(longestPalindrome("bbb"));
         // "abbbba"
