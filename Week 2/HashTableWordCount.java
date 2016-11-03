0a99bb3423b20b11a2ad5f5c1c97d5686cec9c19
khoa <dangkhoa141@gmail.com>
2016-11-03 16:56:26 -0400

    Add invert map challenge 5

--- Week 2/HashTableWordCount.java
+++ Week 2/HashTableWordCount.java
@@ -1,11 +1,15 @@
 import java.util.*;
 public class HashTableWordCount {
 	public static void main(String[] args) {
-		System.out.println(wordCount("To be or not to be, that is the question"));
+		HashMap<String, Integer> wordMap = wordCount("To be or not to be, that is the question");
+		System.out.println(wordMap);
+		Map<Integer, HashSet<String>> invertMap = invertMap(wordMap);
+		System.out.println(invertMap);
+
 	}
-	public static Map<String, Integer> wordCount(String string) {
+	public static HashMap<String, Integer> wordCount(String string) {
 		String[] words = string.split("\\W");
-		Map<String, Integer> wordMap = new HashMap<>();
+		HashMap<String, Integer> wordMap = new HashMap<>();
 		for (String word : words) {
 			word = word.trim().toLowerCase();
 			if (word.length()==0) continue;
@@ -14,5 +18,16 @@ public class HashTableWordCount {
 			wordMap.put(word, wordMap.get(word)+1);
 		}
 		return wordMap;
+	}
+	public static Map<Integer, HashSet<String>> invertMap(HashMap<String, Integer> map) {
+		Map<Integer, HashSet<String>> multimap = new HashMap<Integer, HashSet<String>>();
+		for (String word : map.keySet()) {
+			int val = map.get(word);
+			if (!multimap.containsKey(val))
+				multimap.put(val, new HashSet<String>());
+			multimap.get(val).add(word);
+		}
+		return multimap;
+
 	}
 }
